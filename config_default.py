ff49f334c1e426a9a0dab9752d924d573668657c
mengdechao <mengdechaolive@qq.com>
2015-12-05 00:10:12 +0800

    hello2

---
+++ config_default.py
@@ -0,0 +1,20 @@
+configs = {
+    'db':{
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'root',
+        'password':'123456',
+        'database':'jblog'
+    }, 
+    'session':{
+        'secret':'fanfubuaa'    
+    },
+    'review':{
+        'domain':'fanfubuaa.sinaapp.com',
+        'url':'http://v2.uyan.cc/code/uyan.js?uid=2029306'
+        },
+    'storage':{
+        'bucket':'fanfubuaa'
+        }
+        
+}
